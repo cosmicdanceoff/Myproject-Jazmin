f7a59d05dacf3257665627ae5ab1bd9bc7405455
cosmicdanceoff <ona.acuna.j@gmail.com>
2018-08-21 14:14:17 +0200

    table added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function init() {
     Tabletop.init({
-      key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+      //key: 'https://docs.google.com/spreadsheets/d/14GTxRdrZgGe2sLrxX-KErQnWuNR2St25qgJHZXLNrhk/pubhtml',
+      key: 'https://docs.google.com/spreadsheets/d/1hg4e7Tmwuec9D733FZ_iqSKFlLVKx9z9jyvSf5NtkD4/pubhtml',
       callback: function (tabletopData, tabletop) {
         console.log(tabletopData)
    
