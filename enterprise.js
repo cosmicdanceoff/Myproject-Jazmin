76537e01fd9d9335180dcd6eed5d5d6d707fbffc
cosmicdanceoff <ona.acuna.j@gmail.com>
2018-08-28 09:43:35 +0200

    added more comprehensible code to enterprise.js

--- enterprise.js
+++ enterprise.js
@@ -1,36 +1,29 @@
 workers = [
-    { name: "paul", alive: true, salary: 5000 },
-    { name: "rob", alive: false, salary: 3000 },
-    { name: "anton", alive: false, salary: 5000 },
-    { name: "steven", alive: true, salary: 2000 },
-    { name: "tom", alive: true, salary: 10000 },
-   ]
-   
-   // send a card to every dead workers family
-   function sendCards(workers) {
-    for (var i = 0; i < workers.length; i++) {
-      var worker = workers[i];
-      if (worker.alive == false) {
-        console.log("sending card to the beloved family of", worker.name)
-      }
+  { name: "paul", alive: true, salary: 5000 },
+  { name: "rob", alive: false, salary: 3000 },
+  { name: "anton", alive: false, salary: 5000 },
+  { name: "steven", alive: true, salary: 2000 },
+  { name: "tom", alive: true, salary: 10000 },
+ ]
+
+ // send a card to every dead workers family
+ function sendCards(workers) {
+  for (var i = 0; i < workers.length; i++) {
+    var worker = workers[i];
+    if (worker.alive == false) {
+      console.log("sending card to the beloved family of", worker.name)
     }
-   }
-   
-   // totale loonkost per maand
-   function loonkost(workers) {
-    var total = 0
-    for (var i = 0; i < workers.length; i++) {
-      var worker = workers[i];
-      if (worker.alive) {
-        total = total + worker.salary
-      }
+  }
+ }
+
+ // totale loonkost per maand
+ function loonkost(workers) {
+  var total = 0
+  for (var i = 0; i < workers.length; i++) {
+    var worker = workers[i];
+    if (worker.alive) {
+      total = total + worker.salary
     }
-    return total
-   }
-   
-   function gemiddelde(workers) {
-    return 4000
-   }
-   
-   sendCards(workers)
-   console.log(loonkost(workers))
+  }
+  return total
+ }
