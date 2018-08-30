1439e329084760ff0ab6b0e7d690ce89eef74327
cosmicdanceoff <ona.acuna.j@gmail.com>
2018-08-30 12:04:04 +0200

    cow rendering part 1

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+var cows = [
+  { breed: "mlkmlkmlk", origin: "mlkjmlkmlk" },
+]
+
+function renderCows(cows) {
+  // return <tbody>
+  var tr = document.createElement("tr")
+  var td = document.createElement("td") 
+
+  div.appendChild(tr)
+  div.appendChild(td)
+}
+
+function renderCow(cow) {
+       // return <tr>
+       var newElement = document.createElement(cow.orgin)
+       var textNode = document.createTextNode(cow.breed)
+       newElement.appendChild(textNode)
+  
+       return newElement
+}
+
+
+
+/*
+console.log(renderCow(cow[0]))
+console.log(renderCows(cows))
+*/
+
+
+
+
+
+
+
+
+
+
+
 function init() {
     Tabletop.init({
       //key: 'https://docs.google.com/spreadsheets/d/14GTxRdrZgGe2sLrxX-KErQnWuNR2St25qgJHZXLNrhk/pubhtml',
